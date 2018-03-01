b4fde64174bdf2dc344cf5ae50c641728890bf66
pereznancy <nan.per9@outlook.com>
2018-02-28 19:48:32 -0800

    changes

--- js/app.js
+++ js/app.js
@@ -18,12 +18,31 @@ var imagesArray = [
   'images/wine_glass.jpg'
 ];
 
+//select a random picture from the array and store it in the variable
 var getRandomPicture = imagesArray[Math.floor(Math.random()*imagesArray.length)];
-;
-//will display image on website
-document.body.innerHTML= "<img src='" + getRandomPicture + "'>";
-
-for (var index = 0; index < imagesArray.length; index++) {
-  var item = getRandomItem(imagesArray);
-  images[i].innerHTML = '<img src=' + imagesArray[i];
+//use varible to send picture via inner.HTML for simplicity
+function showPictures() {
+  var pic = document.body.innerHTML = "<img src='" + getRandomPicture + "'>";
+  pic
 }
+showPictures();
+
+// document.getElementbyID("images").innerHTML = "<img src='" + getRandomPicture + "'>";
+// document.getElementbyID("images").innerHTML = "<img src='" + getRandomPicture + "'>";
+
+//
+// function getThreePictures() {
+//   var item = getRandomPicture;
+//   var index = 1
+//   while (index < 4) {
+//
+//   }
+// }
+//
+// function getThreePictures() {
+//   for (var index = 0; index < imagesArray.length; index++) {
+//     var item = getRandomPicture;
+//     document.getElementbyID("images").innerHTML = "'<img src='" + item + "'>" ;
+//   }
+// }
+// getThreePictures();
