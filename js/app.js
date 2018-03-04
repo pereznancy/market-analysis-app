f0ef181350f01a803a40ea58c0cbe67a8672f243
pereznancy <nan.per9@outlook.com>
2018-03-04 15:04:01 -0800

    click counter

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var trackClick = function(name, location) { //info will be pulled from array
   this.vote = 0;
 }
 
+function addListener() {
+  document.getElementById("images").addEventListener("click", newClicks)
+}
 //array of all the images w/name and location
 //with new trackClick, it will send info to function above ^
 var imagesArray = [
@@ -27,6 +30,9 @@ var imagesArray = [
 
 //select a random picture from the array and store it in the variable
 function getRandomPictures() {
+  var voteTracker = 0
+  var sendImage = document.getElementById("images");
+  sendImage.innerHTML = "";
   var chosen = [];  //array to hold the 3 pictures
   for (var index = 0; index <3; index++) { //only need to get up to 3 images
     chosen[index] = imagesArray[Math.floor(Math.random()*imagesArray.length)];
@@ -37,25 +43,28 @@ function getRandomPictures() {
   }
   for (var newIndex = 0; newIndex < 3; newIndex++) {
     var firstImage = document.createElement("img");
-    var sendImage = document.getElementById("images");
     firstImage.src = chosen[newIndex].location;
     sendImage.appendChild(firstImage);
+    voteTracker += 1;
   }
 }
 
-window.addEventListener("load", getRandomPictures)
+//saves votes if clicked
+function newClicks(event) {
+  if (event.target.tagName == "IMG") {
+    var index = event.target.src.lastIndexOf("/");
+    var imageLocation = "images/" + event.target.src.substring(index + 1);
+    console.log(imageLocation);
+    for (var indexClick = 0; indexClick < imagesArray.length; indexClick++)
+      if (imagesArray[indexClick].location.indexOf(imageLocation) != -1) {
+        imagesArray[indexClick].vote += 1;
+        console.log(imagesArray[indexClick].vote);
+      }
+  }
+  getRandomPictures();
+}
 
-// document.getElementById("images").addEventListener("click", recordClick);
-// window.addEventListener();
 
 
-
-
-
-// function getThreePictures() {
-//   for (var index = 0; index < imagesArray.length; index++) {
-//     var item = getRandomPicture;
-//     document.getElementbyID("images").innerHTML = "'<img src='" + item + "'>" ;
-//   }
-// }
-// getThreePictures();
+window.addEventListener("load", getRandomPictures);
+window.addEventListener("load", addListener);
