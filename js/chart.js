ac541a95ec5788dc9057c19bb0cb410b00b23ac2
William Tronvig <william.tronvig@gmail.com>
2018-03-04 16:07:35 -0800

    paired programming successful

---
+++ js/chart.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function(){
+    document.getElementById("show-chart-button").addEventListener("click", showChart);
+})
+
+function showChart () {
+	var chart = new CanvasJS.Chart("chartContainer", {
+		title:{
+			text: "BusMall Image Results"              
+		},
+		data: [              
+		{
+			// Change type to "doughnut", "line", "splineArea", etc.
+			type: "column",
+			dataPoints: imagesArray
+		}
+		]
+	});
+	chart.render();
+}
