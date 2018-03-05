f2c7994d4a338d0b7b1e399ff0dd046ffe32fefa
pereznancy <nan.per9@outlook.com>
2018-03-04 17:50:33 -0800

    working list

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var trackClick = function(name, location) { //info will be pulled from array
   this.vote = 0;
 }
 
+function addListener() {
+  document.getElementById("images").addEventListener("click", newClicks)
+}
 //array of all the images w/name and location
 //with new trackClick, it will send info to function above ^
 var imagesArray = [
@@ -27,35 +30,58 @@ var imagesArray = [
 
 //select a random picture from the array and store it in the variable
 function getRandomPictures() {
+  var voteTracker = 0
+  var sendImage = document.getElementById("images");
+  sendImage.innerHTML = "";
   var chosen = [];  //array to hold the 3 pictures
   for (var index = 0; index <3; index++) { //only need to get up to 3 images
     chosen[index] = imagesArray[Math.floor(Math.random()*imagesArray.length)];
   }
   if (chosen[0] === chosen[1] || chosen[0] === chosen[2] || chosen[1] === chosen[2]) { //make sure no imposters aka duplicates
     var location = imagesArray[index].location;
-    chosen = getRandomPictures(); //run through randomizer again to pick another random picture so no dupes
-  }
-  for (var newIndex = 0; newIndex < 3; newIndex++) {
-    var firstImage = document.createElement("img");
-    var sendImage = document.getElementById("images");
-    firstImage.src = chosen[newIndex].location;
-    sendImage.appendChild(firstImage);
+    getRandomPictures(); //run through randomizer again to pick another random picture so no dupes
+  } else {
+    for (var newIndex = 0; newIndex < 3; newIndex++) {
+      var firstImage = document.createElement("img");
+      firstImage.src = chosen[newIndex].location;
+      sendImage.appendChild(firstImage);
+      voteTracker += 1;
+    }
   }
 }
 
-window.addEventListener("load", getRandomPictures)
+//saves votes if clicked
+function newClicks(event) {
+  if (event.target.tagName == "IMG") {
+    var index = event.target.src.lastIndexOf("/");
+    var imageLocation = "images/" + event.target.src.substring(index + 1);
+    console.log(imageLocation);
+    for (var indexClick = 0; indexClick < imagesArray.length; indexClick++)
+      if (imagesArray[indexClick].location.indexOf(imageLocation) != -1) {
+        imagesArray[indexClick].vote += 1;
+        console.log(imagesArray[indexClick].vote);
+      }
+  }
+  getRandomPictures();
+}
 
-// document.getElementById("images").addEventListener("click", recordClick);
-// window.addEventListener();
+
+function makeList() {
+  var listContainer = document.getElementById("list-container");
+  var resultsList = "<h3>Results</h3>" + "<ul>";
+  for (var index = 0; index < imagesArray.length; index++) {
+    var picture = imagesArray[index].name;
+    resultsList += "<li>" + picture + ":" + imagesArray[index].vote + "</li>";
+  }
+  listContainer.innerHTML += resultsList + "</ul>";
+}
+
+function addList() {
+    document.getElementById("show-list-button").addEventListener("click", makeList);
+}
 
 
 
-
-
-// function getThreePictures() {
-//   for (var index = 0; index < imagesArray.length; index++) {
-//     var item = getRandomPicture;
-//     document.getElementbyID("images").innerHTML = "'<img src='" + item + "'>" ;
-//   }
-// }
-// getThreePictures();
+window.addEventListener("load", getRandomPictures);
+window.addEventListener("load", addListener);
+window.addEventListener("load", addList);
