c9aa0dc98b4282ee0c74fb53a7d77bd8b7aa2c21
pereznancy <nan.per9@outlook.com>
2018-03-05 20:24:24 -0800

    fixes

--- js/chart.js
+++ js/chart.js
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
-// window.addEventListener("load", function(){
-//     document.getElementById("show-chart-button").addEventListener("click", showChart);
-// })
-=======
-window.addEventListener("load", function(){
-    document.getElementById("show-chart-button").addEventListener("click", showChart);
-})
->>>>>>> 4046618567e797bcaf037be5704f86898c825f72
 
 function showChart () {
 	var chart = new CanvasJS.Chart("chartContainer", {
 		title:{
-<<<<<<< HEAD
 			text: "BusMall Image Results"
 		},
 		data: [
-=======
-			text: "BusMall Image Results"              
-		},
-		data: [              
->>>>>>> 4046618567e797bcaf037be5704f86898c825f72
 		{
 			// Change type to "doughnut", "line", "splineArea", etc.
 			type: "column",
